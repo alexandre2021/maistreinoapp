2d824acdcaac35d876a893e70c46b90108dcaf46
Alexandre Ramos <aramos1069gmail.com>
2025-07-04 10:12:19 -0300

    style: Ajusta formatação dos imports em configuracao.tsx

--- app/criar-rotina/configuracao.tsx
+++ app/criar-rotina/configuracao.tsx
@@ -3,15 +3,15 @@ import { Ionicons } from '@expo/vector-icons';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import React, { useEffect, useState } from 'react';
 import {
-  Alert,
-  KeyboardAvoidingView,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View
+    Alert,
+    KeyboardAvoidingView,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View
 } from 'react-native';
 
 // Components
